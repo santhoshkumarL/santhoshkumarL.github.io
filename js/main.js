97380cf38b1f521f78aabe059e81902cd47aa3d3
santhosh <santhosh@hamsind.com>
2017-05-23 19:02:16 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// var quotes = [
+//         "That's one small step for a man, one giant leap for mankind - Neil Armstrong",
+//         "The Universe is under no obligation to make sense to you - Neil deGrasse Tyson",
+//         "I would like to die on Mars. Just not on impact - Elon Musk",
+//         "If we can conquer space, we can conquer childhood hunger - Buzz Aldrin",
+//         "The eternal silence of these infinite spaces frightens me - Blaise Pascal"
+// ];
+
+var quoteArray = new Vue({
+  el: '#quoteArray',
+  data: {
+    quotes: [
+        "That's one small step for a man, one giant leap for mankind - Neil Armstrong",
+        "The Universe is under no obligation to make sense to you - Neil deGrasse Tyson",
+        "I would like to die on Mars. Just not on impact - Elon Musk",
+        "If we can conquer space, we can conquer childhood hunger - Buzz Aldrin",
+        "The eternal silence of these infinite spaces frightens me - Blaise Pascal"
+	]
+  }
+})
